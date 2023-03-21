4884a74511c8d69ef396a5efb8fb9759dc9f1ac2
Max McDonough <max.mcdonough@gmail.com>
2023-03-21 15:18:00 -0400

    Tweaked header styling

--- view-from-the-sky/src/components/Header/styles.ts
+++ view-from-the-sky/src/components/Header/styles.ts
@@ -7,25 +7,22 @@ export const HeaderBar = styled.header`
   flex-direction: column;
   justify-content: space-around;
   background-position: center;
-  background-color: #000;
-  width: 100.9%;
+  background-color: #FFF;
+  width: 100%;
   @media (min-width: 600px) {
     flex-direction: row;
   }
 `;
 
 export const Title = styled.h1`
-  color: #FFF;
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  font-size: 12vw;
-  font-style: italic;
+  color: #000;
+  font-family: 'Futura', 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  font-size: clamp(16px, 8vw, 30px);
   opacity: 90%;
-  text-shadow: 3px 3px 3px black;
-  width: 100%;
+  text-shadow: 1px 1px 1px black;
+  width: 80%;
   @media (min-width: 600px) {
-    font-size: 6vh;
-    flex-direction: row;
-    width: 40%;
+    flex-direcion: row;
   }
 `;
 
