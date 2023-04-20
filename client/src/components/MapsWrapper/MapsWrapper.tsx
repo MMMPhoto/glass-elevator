c6abe3835d96978ade1da5613ac21e3548f88e2e
Max McDonough <max.mcdonough@gmail.com>
2023-04-19 22:39:31 -0400

    inline switch statement for user location permission change

--- client/src/components/MapsWrapper/MapsWrapper.tsx
+++ client/src/components/MapsWrapper/MapsWrapper.tsx
@@ -17,10 +17,11 @@ const markers: any | null = markerData;
 const MapsWrapper = () => {
   const [map, setMap] = useState<GoogleMap>();
 
-  const [userLocation, setUserLocation] = useState<Position>({ lat: 30, lng: -90 });
-  const [searchPlace, setSearchPlace] = useState<Position>({ lat: 0, lng: 0 });
+  const [locationPermissionStatus, setLocationPermissionStatus] = useState<string>("prompt");
+  const [userLocation, setUserLocation] = useState<LatLng>({ lat: 30, lng: -90 });
+  const [searchPlace, setSearchPlace] = useState<LatLng>({ lat: 0, lng: 0 });
 
-  const [mapCenter, setMapCenter] = useState<Position>({ lat: 30, lng: -90 });
+  const [mapCenter, setMapCenter] = useState<LatLng>({ lat: 30, lng: -90 });
   const [mapZoom, setMapZoom] = useState<number>(4);
   const [activeMarker, setActiveMarker] = useState<any>();
 
@@ -31,66 +32,85 @@ const MapsWrapper = () => {
   };
 
   const getUserLocation = () => {
-  // if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position: any) => {
-      const userLocation: Position = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      setUserLocation(userLocation);
-    });
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position: any) => {
+        const userLocation: LatLng = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        setUserLocation(userLocation);
+        // console.log(locationStatus);
+      });
+    } else {
+      // console.log(locationStatus);
+      handlePermission();
+    };
   };
 
-  // navigator.permissions.query({ name: "geolocation" }).then((permissionStatus) => {
-  //   permissionStatus.onchange = () => {
-  //     setLocationAccess(permissionStatus.state == "granted");
-  //     if (permissionStatus.state=="granted") {
-  //       getUserLocation();
-  //     }
-  //   };
-  // });
+  const handlePermission = () => {
+    navigator.permissions.query({ name: "geolocation" }).then((permissionStatus) => {
+      setLocationPermissionStatus(permissionStatus.state);
+      permissionStatus.onchange = () => {
+        setLocationPermissionStatus(permissionStatus.state);
+      }
+    });
+    //   if (permissionStatus.state == "propmt") {
+    //     handlePermission();
+    //   } else if {
+    //     ()
+    //   }
+    //   setLocationStatus(permissionStatus.state)
+    //   handlePermission();
+    // });
+  };
 
   useEffect(() => {
+    handlePermission();
     getUserLocation();
   }, []);
 
   return (
     <Wrapper apiKey={apiKey} render={render} libraries={['places']}>
-      <Search 
-        setMapCenter={setMapCenter}
-        setMapZoom={setMapZoom}
-        map={map!}
-      />
-      <Map
-        map={map!}
-        setMap={setMap}
-        center={mapCenter} 
-        zoom={4}
-      >
-        {markers
-          ? markers.map((marker: any, index: number) => (
-            <Marker
-              key={index}
-              map={map!}
-              markerData={marker}
-              position={{ lat: marker.lat, lng: marker.lng }}
-              setActiveMarker={setActiveMarker}
-            />
-            ))
-          : null
-          }
-        {activeMarker
-          ? <InfoWindow 
-            marker={activeMarker}
-            />
-          : <></>
-        }
-      </Map>
+      {{ // Switch statment based on browser's location permission status
+        "prompt": <p>Please approve your location! It works better :)</p>,
+        "denied": <div>
+                    <p>We can't find your location. Search by a location:</p>
+                    <Search 
+                        setMapCenter={setMapCenter}
+                        setMapZoom={setMapZoom}
+                        map={map!}
+                      />
+                    <Map
+                      map={map!}
+                      setMap={setMap}
+                      center={mapCenter} 
+                      zoom={4}
+                    >
+                      {markers
+                        ? markers.map((marker: any, index: number) => (
+                          <Marker
+                            key={index}
+                            map={map!}
+                            markerData={marker}
+                            position={{ lat: marker.lat, lng: marker.lng }}
+                            setActiveMarker={setActiveMarker}
+                          />
+                          ))
+                        : null
+                        }
+                      {activeMarker
+                        ? <InfoWindow 
+                          marker={activeMarker}
+                          />
+                        : <></>
+                      }
+                    </Map>
+                  </div>,
+          "granted": <p>Getting your location...</p>
+      }[locationPermissionStatus]
+      }
     </Wrapper>
   )
 };
 
 export default MapsWrapper;
-
-
-
